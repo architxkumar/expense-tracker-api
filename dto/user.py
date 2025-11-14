904f44b57571ddee75690d748754a5d28cdbf579
Archit Kumar <architxkumar@proton.me>
2025-11-14 20:35:42 +0530

    feat: add validation to `UserCreate` DAO

--- dto/user.py
+++ dto/user.py
@@ -1,9 +1,10 @@
-from sqlmodel import SQLModel
+from pydantic import EmailStr
+from sqlmodel import SQLModel, Field
 
 
 class UserCreate(SQLModel):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
-
+    model_config = {"extra": "forbid"}
+    first_name: str = Field(max_length=100)
+    last_name: str = Field(max_length=100)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=100)
